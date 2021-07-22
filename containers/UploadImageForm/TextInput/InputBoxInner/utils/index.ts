124cea1cfbe1ef17f2c50567952ed8b24b30ee25
Caleb Taylor <aquaductape@gmail.com>
2021-07-22 00:34:54 -0700

    update

--- containers/UploadImageForm/TextInput/InputBoxInner/utils/index.ts
+++ containers/UploadImageForm/TextInput/InputBoxInner/utils/index.ts
@@ -32,91 +32,6 @@ export const splitValueIntoUrlItems = ({ value }: { value: string }) => {
     }
   );
 
-  const getYoutubeImgFromURL = (url: string) => {
-    // regex https://stackoverflow.com/a/27728417/8234457
-    // images from id https://stackoverflow.com/a/20542029/8234457
-    const youtubeIdRegex =
-      /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
-    const result = url.match(youtubeIdRegex)!;
-
-    if (!result) return null;
-
-    const id = result[1];
-
-    if (id) {
-      return {
-        id,
-        url: `https://img.youtube.com/vi/${id}/0.jpg`,
-      };
-    }
-
-    return null;
-  };
-
-  const getImgFromSearchEngineImageResult = (inputURL: string) => {
-    const google = "www.google.com/imgres?";
-    const bing = "www.bing.com/images/search?";
-    const yahoo = "images.search.yahoo.com/images";
-    const baidu = "image.baidu.com/search/detail?";
-
-    type SE = {
-      [key: string]: { url: string; param: string; prependProtocol?: boolean };
-    };
-
-    const searchEngines: SE = {
-      google: {
-        url: google,
-        param: "imgurl",
-      },
-      bing: {
-        url: bing,
-        param: "mediaurl",
-      },
-      yahoo: {
-        url: yahoo,
-        param: "imgurl",
-        prependProtocol: true,
-      },
-      baidu: {
-        url: baidu,
-        param: "objurl",
-      },
-    };
-
-    for (const key in searchEngines) {
-      const { url, param, prependProtocol } = searchEngines[key];
-      const newURL = url.replace(/\./g, "\\.").replace(/\//g, `\\\/`);
-      const regex = new RegExp(`^https?:\\/\\/${newURL}(.+)${param}=([^&]+)`);
-      const regexResult = inputURL.match(regex);
-      const getProtocol = (url: string) => {
-        const result = url.match(/url=(https?)/);
-        const fallback = "http://"; // must fallback to unsecure protocol since secure can fallback to that, but unsecure protocol can't use secure
-
-        if (!result) return fallback;
-
-        const protocol = result[1];
-
-        if (!protocol) return fallback;
-
-        return protocol + "://";
-      };
-
-      if (!regexResult) continue;
-      let imgURL = regexResult[2];
-
-      if (!imgURL) continue;
-
-      if (prependProtocol) {
-        const protocol = getProtocol(inputURL);
-        imgURL = protocol + imgURL;
-      }
-
-      return decodeURIComponent(imgURL);
-    }
-
-    return null;
-  };
-
   const urls = value
     .split(/\s+|\n+/)
     .filter((item) => item.match(urlTypesRegex));
@@ -126,6 +41,7 @@ export const splitValueIntoUrlItems = ({ value }: { value: string }) => {
     let isDataURL = !!url.match(/^data:image.+base64,/);
     let error = false;
     let errorMsg = "";
+    let name = "";
 
     if (isDataURL) {
       const objectURL = convertDataURLToObjectURL(url);
@@ -137,17 +53,13 @@ export const splitValueIntoUrlItems = ({ value }: { value: string }) => {
         url = objectURL;
       }
     }
-    const youtubeData = getYoutubeImgFromURL(url);
-    const searchEngineResult = getImgFromSearchEngineImageResult(url);
 
-    if (searchEngineResult) {
-      url = searchEngineResult;
-    }
-
-    const name = youtubeData ? youtubeData.id : getImageNameFromUrl(url);
-
-    if (youtubeData) {
-      url = youtubeData.url;
+    if (isDataURL || isObjectURL) {
+      name = getImageNameFromUrl(url);
+    } else {
+      const result = getImgDataFromLink(url);
+      name = result.name;
+      url = result.url;
     }
 
     return {
@@ -187,4 +99,110 @@ export const checkDebouncedUrls = async (
       dispatch(setUrlItemError({ id, error, isDataURL }));
     });
   });
+};
+
+const getYoutubeImgFromURL = (url: string) => {
+  // regex https://stackoverflow.com/a/27728417/8234457
+  // images from id https://stackoverflow.com/a/20542029/8234457
+  const youtubeIdRegex =
+    /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+  const result = url.match(youtubeIdRegex)!;
+
+  if (!result) return null;
+
+  const id = result[1];
+
+  if (id) {
+    return {
+      id,
+      url: `https://img.youtube.com/vi/${id}/0.jpg`,
+    };
+  }
+
+  return null;
+};
+
+const getImgFromSearchEngineImageResult = (inputURL: string) => {
+  const google = "www.google.com/imgres?";
+  const bing = "www.bing.com/images/search?";
+  const yahoo = "images.search.yahoo.com/images";
+  const baidu = "image.baidu.com/search/detail?";
+
+  type SE = {
+    [key: string]: { url: string; param: string; prependProtocol?: boolean };
+  };
+
+  const searchEngines: SE = {
+    google: {
+      url: google,
+      param: "imgurl",
+    },
+    bing: {
+      url: bing,
+      param: "mediaurl",
+    },
+    yahoo: {
+      url: yahoo,
+      param: "imgurl",
+      prependProtocol: true,
+    },
+    baidu: {
+      url: baidu,
+      param: "objurl",
+    },
+  };
+
+  for (const key in searchEngines) {
+    const { url, param, prependProtocol } = searchEngines[key];
+    const newURL = url.replace(/\./g, "\\.").replace(/\//g, `\\\/`);
+    const regex = new RegExp(`^https?:\\/\\/${newURL}(.+)${param}=([^&]+)`);
+    const regexResult = inputURL.match(regex);
+    const getProtocol = (url: string) => {
+      const result = url.match(/url=(https?)/);
+      const fallback = "http://"; // must fallback to unsecure protocol since secure can fallback to that, but unsecure protocol can't use secure
+
+      if (!result) return fallback;
+
+      const protocol = result[1];
+
+      if (!protocol) return fallback;
+
+      return protocol + "://";
+    };
+
+    if (!regexResult) continue;
+    let imgURL = regexResult[2];
+
+    if (!imgURL) continue;
+
+    if (prependProtocol) {
+      const protocol = getProtocol(inputURL);
+      imgURL = protocol + imgURL;
+    }
+
+    return decodeURIComponent(imgURL);
+  }
+
+  return null;
+};
+
+const getImgDataFromLink = (url: string) => {
+  const searchEngineResult = getImgFromSearchEngineImageResult(url);
+
+  if (searchEngineResult) {
+    url = searchEngineResult;
+  }
+
+  const youtubeData = getYoutubeImgFromURL(url);
+
+  const name = youtubeData ? youtubeData.id : getImageNameFromUrl(url);
+
+  if (youtubeData) {
+    url = youtubeData.url;
+  }
+
+  return {
+    url,
+    name,
+  };
 };
